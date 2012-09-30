3547b4cb9b3a76344f0691e75484345bc65335e5
Alex Asher <alex.asher@gmail.com>
2012-09-30 17:46:18 +1300

    My Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StrongFriendsApp::Application.config.secret_token = 'db1b37a91f20b64ea9f3db0cf9efe24b1e55aa447258eb39001c5c2b7884543188485e49e41050554b3cdc90f0060591c46fb506edd0a21210cafaa09bc945c2'
